1eaeffc07083edc5e168c4db91ab1afcd598cc40
20152010 <20152010@tafe.wa.edu.au>
2025-11-19 12:07:11 +0800

    Added config.json

--- src/car_park.py
+++ src/car_park.py
@@ -2,6 +2,7 @@ from sensor import Sensor
 from display import Display
 from pathlib import Path
 from datetime import datetime
+import json
 class CarPark:
 
 
@@ -52,3 +53,15 @@ class CarPark:
     def _log_car_activity(self, plate, action):
         with self.log_file.open("a") as f:
             f.write(f"{plate} {action} at {datetime.now(): %Y-%m-%d %H:%M:%S}\n")
+
+    def write_config(self):
+        with open("config.json", "w") as f:
+            json.dump({"location": self.location,
+                       "capacity": self.capacity,
+                       "log_file": str(self.log_file)}, f)
+    @classmethod
+    def from_config(cls, config_file=Path("config.json")):
+        config_file = config_file if isinstance(config_file, Path) else Path(config_file)
+        with config_file.open() as f:
+            config = json.load(f)
+        return cls(config["location"], config["capacity"], log_file=config["log_file"])
