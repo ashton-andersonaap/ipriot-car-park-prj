1a4f4e311e561b9d27adb3bb3e69a8ca8379d529
20152010 <20152010@tafe.wa.edu.au>
2025-11-19 10:31:50 +0800

    Created update car park, scan plates and detected vehicle methods in sensor.py

--- src/sensor.py
+++ src/sensor.py
@@ -1,4 +1,6 @@
-class Sensor:
+from abc import ABC, abstractmethod
+import random
+class Sensor(ABC):
     def __init__(self, id, is_active, car_park):
         self.id = id
         self.is_active = is_active
@@ -8,8 +10,27 @@ class Sensor:
         return f"Sensor {self.id} {self.is_active}"
 
 class EntrySensor(Sensor):
-    pass
+
+    def update_car_park(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Incoming vehicle detected. Plate: {plate}")
+
 
 class ExitSensor(Sensor):
+    def update_car_park(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Outgoing vehicle detected. Plate: {plate}")
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
+
+@abstractmethod
+def update_car_park(self, plate):
     pass
+
+def _scan_plate(self):
+    return "FAKE-" +format(random.randint(0,999), "03d")
+
+def detect_vehicle(self):
+    plate = self._scan_plate()
+    self.update_car_park(plate)
 
